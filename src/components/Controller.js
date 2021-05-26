7b3f45f96d1a163fd83498b4915f045518c515c2
Eric Trinh <erictrinh153@gmail.com>
2021-05-26 14:39:43 +1000

    removing the redundant styling file

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -20,12 +20,12 @@ export function isSolved(tiles) {
   return true;
 }
 
-// Get the linear index from a row/col pair.
+// Get the index from a row/col pair.
 export function getIndex(row, col) {
   return parseInt(row, 10) * GRID_SIZE + parseInt(col, 10);
 }
 
-// Get the row/col pair from a linear index.
+// Get the row/col pair from a index.
 export function getMatrixPosition(index) {
   return {
     row: Math.floor(index / GRID_SIZE),
